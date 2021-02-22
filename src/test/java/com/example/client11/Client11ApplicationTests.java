60f7fa74f5595449cfb73a1c8439ad38c1739e12
Oleg <kandalov.o.a@gmail.com>
2021-02-22 14:47:06 +0200

    Initial commit

---
+++ src/test/java/com/example/client11/Client11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.client11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Client11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
